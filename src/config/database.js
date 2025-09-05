f71f0ab98c0bb44686df922005c8da766ff74a99
gledson <gledsonalcantara4@gmail.com>
2025-09-05 12:49:03 -0400

    atualizaçâo no env

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
-export default {
-  development: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-    },
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   },
-};
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+});
+
+export default sequelize;
