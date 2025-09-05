64ed6700196ddf21816782ae0e6fee672a2e87db
gledson <gledsonalcantara4@gmail.com>
2025-09-05 12:26:39 -0400

    atualizado

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,13 @@
-/* eslint-disable prettier/prettier */
-import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // cuidado: em produção, o ideal é usar um certificado válido
-    }
+export default {
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
-});
-
-export default sequelize;
+};
